351a6fa4986a628c75dffe35cc50549b5ee4a977
Fusibu <ollerexpositomartin@gmail.com>
2022-11-12 20:04:46 +0100

    added proxy for add cors

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import Auth from './routes/auth'
 import bodyParser from "body-parser";
+import { proxy } from "./proxy";
 
 const app = express();
 const port = 8080;
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(proxy);
 app.use(bodyParser.json());
-// app.use(bodyParser.raw());
-// app.use(bodyParser.text())
+app.use(bodyParser.raw());
+app.use(bodyParser.text())
 
 app.use('/auth',Auth)
 
-
 app.listen( process.env.PORT || 8080, () => {
     console.log( `server started at http://localhost:${ port } 🚀`);
-} );
+});
