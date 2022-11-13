3a73476cd7bdb7657f9a1122384249abbf874b52
Fusibu <ollerexpositomartin@gmail.com>
2022-11-13 09:35:05 +0100

    compile proxy

--- dist/index.js
+++ dist/index.js
@@ -6,25 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const proxy_1 = require("./proxy");
 const app = (0, express_1.default)();
 const port = 8080;
 app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(proxy_1.proxy);
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.raw());
 app.use(body_parser_1.default.text());
-app.use((req, res, next) => {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    // Pass to next layer of middleware
-    next();
-});
 app.use('/auth', auth_1.default);
 app.listen(process.env.PORT || 8080, () => {
     console.log(`server started at http://localhost:${port} 🚀`);
---
+++ dist/proxy.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.proxy = void 0;
+const proxy = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // Pass to next layer of middleware
+    next();
+};
+exports.proxy = proxy;
+//# sourceMappingURL=proxy.js.map
